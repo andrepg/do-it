f76317850de0149262c5f85e5ad8ff32fa040bc6
André Paul Grandsire <andrepg@users.noreply.github.com>
2025-07-28 20:19:56 -0300

    create new list store

--- src/utils/list-store.js
+++ src/utils/list-store.js
@@ -1,12 +1,29 @@
+import GObject from "gi://GObject";
 import Gio from "gi://Gio"
+import { Task } from "../ui-handler/task.js";
 
-export class TaskListStore extends Gio.ListStore 
+export const TaskListStore = GObject.registerClass({
+    GTypeName: "TaskListStore",
+    Properties: {},
+    InternalChildren: [],
+    Signals: {},
+}, class TaskListStoreObject extends Gio.ListStore 
 {
-    constructor(params) {
-        super.constructor(params)
-    }
+    _init() {
+        super._init({ item_type: Task });
+    }    
 
-    get count() {
+    get_all() {
+        const items = [];
+
+        for (let index = 0; index < this.get_count(); index++) {
+            items.push(this.get_item(index).to_object());
+        }
+
+        return items;
+    }
+    
+    get_count() {
         return this.n_items;
     }
-}
+});
