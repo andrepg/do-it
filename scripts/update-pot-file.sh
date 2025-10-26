d497e7418219add57a894e57ca965183cb0a23ed
André Paul Grandsire <andrepg@users.noreply.github.com>
2025-10-26 12:24:15 -0300

    update translations, finally

--- scripts/update-pot-file.sh
+++ scripts/update-pot-file.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
 ## Generate the POT template file
 # @see https://rmnvgr.gitlab.io/gtk4-gjs-book/application/translation/
 # @see https://docs.elementary.io/develop/writing-apps/our-first-app/translations
@@ -12,4 +15,6 @@ flatpak run --filesystem=host --command=meson org.gnome.Sdk/x86_64/49 compile -C
 
 echo "Updating PO files to translation"
 flatpak run --filesystem=host --command=meson org.gnome.Sdk/x86_64/49 compile -C _build io.github.andrepg.Doit-update-po
+
+echo "Done."
 
