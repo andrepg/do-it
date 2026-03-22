546cfa0360d9833e472ec92da75c482157f648ee
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-03-22 00:03:20 -0300

    break down new-task action

--- src/actions/new-task.ts
+++ src/actions/new-task.ts
@@ -7,43 +7,57 @@ export const newTask = (store: TaskListStore) => {
     const buttonNewTaskId = 'button_new_task';
     const fieldNewTaskId = 'task_new_entry';
 
-    const getNewTaskButton = (
-        window: DoItMainWindow
-    ): Gtk40.Button => window.get_template_child(DoItMainWindow.GType, buttonNewTaskId) as unknown as Gtk40.Button
+    let fieldNewTask: Gtk40.Entry;
 
-    const getNewTaskField = (
-        window: DoItMainWindow
-    ): Gtk40.Entry => window.get_template_child(DoItMainWindow.GType, fieldNewTaskId) as unknown as Gtk40.Entry
+    const get_widget = <T>(window: DoItMainWindow, id: string): T => window.get_template_child(
+        DoItMainWindow.GType,
+        id
+    ) as unknown as T
+
+    const parseProject = (text: string) => {
+        let project = "";
+        let parsedText = text;
+        const projectMatch = text.match(/@(\S+)/);
+
+        if (projectMatch) {
+            const rawProject = projectMatch[1];
+            project = rawProject.charAt(0).toUpperCase() + rawProject.slice(1).toLowerCase();
+            parsedText = text.replace(projectMatch[0], '').trim();
+        }
+
+        return { project, parsedText }
+    }
+
+    const create_task = (text: string) => {
+        const { project, parsedText: title } = parseProject(text);
+
+        store.append_task({
+            title,
+            created_at: new Date().getTime(),
+            project,
+        });
+
+        store.persist_store();
+    }
 
     const setup = (window: DoItMainWindow) => {
-        const buttonNewTask = getNewTaskButton(window);
-
-        const fieldNewTask = getNewTaskField(window);
-
+        const buttonNewTask = get_widget<Gtk40.Button>(window, buttonNewTaskId);
         buttonNewTask.connect('clicked', () => fieldNewTask.grab_focus())
 
-        fieldNewTask.connect('activate', () => {
-            const text = fieldNewTask.get_text().trim();
-            if (text.length == 0) return;
+        fieldNewTask = get_widget<Gtk40.Entry>(window, fieldNewTaskId);
+        fieldNewTask.connect('activate', handle_user_input)
+    }
 
-            let project = "";
-            let parsedText = text;
-            const projectMatch = text.match(/@(\S+)/);
+    const handle_user_input = () => {
+        const text = fieldNewTask.get_text().trim();
 
-            if (projectMatch) {
-                const rawProject = projectMatch[1];
-                project = rawProject.charAt(0).toUpperCase() + rawProject.slice(1).toLowerCase();
-                parsedText = text.replace(projectMatch[0], '').trim();
-            }
+        if (text.length == 0) return;
 
-            store.append_task({
-                title: parsedText,
-                created_at: new Date().getTime(),
-                project,
-            });
-            fieldNewTask.set_text('');
-            showToast(_('Task created'));
-        })
+        create_task(text);
+
+        fieldNewTask.set_text('');
+
+        showToast(_('Task created'));
     }
 
     return {
