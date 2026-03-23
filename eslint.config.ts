4adae867da89d83975014abbe24ec3dd55e0f336
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-03-23 13:52:42 -0300

    format and lint config

---
+++ eslint.config.ts
@@ -0,0 +1,54 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import json from '@eslint/json';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+
+export default defineConfig([
+  {
+    ignores: [
+      '**/*.d.ts',
+      '.agent/**',
+      '.github/**',
+
+      '.vscode/**',
+
+      'flatpak/**',
+      'tests/**',
+      'scripts/**',
+
+      '_build/**',
+      '.flatpak/**',
+      '.flatpak-builder/**',
+      'node_modules/**',
+    ],
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+    plugins: { js },
+    extends: ['js/recommended', prettier],
+    languageOptions: {
+      globals: globals.node,
+    },
+  },
+  tseslint.configs.recommended,
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.jsonc'],
+    plugins: { json },
+    language: 'json/jsonc',
+    extends: ['json/recommended'],
+  },
+  {
+    files: ['**/*.json5'],
+    plugins: { json },
+    language: 'json/json5',
+    extends: ['json/recommended'],
+  },
+]);
