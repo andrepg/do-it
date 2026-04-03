fe0d7541bda78a8db5017742eda7b8bdb4aaf4ac
André Paul Grandsire <andrepg@users.noreply.github.com>
2026-04-03 16:59:36 -0300

    feat: reorganize github workflows

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,25 @@
-export default { extends: ['@commitlint/config-conventional'] };
+export default {
+  extends: ['@commitlint/config-conventional'],
+  helpURL: 'https://www.conventionalcommits.org/',
+  plugins: [],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'lang',
+      ],
+    ],
+  },
+};
